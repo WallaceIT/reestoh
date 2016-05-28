ed6bb12b038232fdcac058dc870d3980b7f0a905
Francesco Valla <valla.francesco@gmail.com>
2016-05-28 16:25:39 +0200

    Config refactoring

--- config.php
+++ config.php
@@ -1,16 +1,18 @@
 <?php
 // DATABASE CONFIGURATION
-$dbname = 'reestoh';
-$dsn = 'mysql:host=localhost;dbname='.$dbname;
-$username = 'root';
-$password = '';
-$options = array(
-    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
-); 
-
-$db = new PDO($dsn, $username, $password, $options) or die("Unable to Connect to the specified DB!");
+$CONFIG_HOSTNAME = 'localhost';
+$CONFIG_DBNAME = 'reestoh';
+$CONFIG_USERNAME = 'root';
+$CONFIG_PASSWORD = '';
 
 // OPTIONS
 $CONFIG_PRINT_RECEIPT = true;
 $CONFIG_PRINT_MODE = 'html'; // can be 'html' or 'pdf'
+
+
+// DATABASE CONNECTION SETUP
+$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
+$dsn = 'mysql:host='.$CONFIG_HOSTNAME.';dbname='.$CONFIG_DBNAME;
+$db = new PDO($dsn, $CONFIG_USERNAME, $CONFIG_PASSWORD, $options) or die("Unable to Connect to the specified DB!");
+
 ?>
