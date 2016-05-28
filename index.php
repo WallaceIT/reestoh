19fa5880c6bda2258c44c1cd3c8b83d57517a26b
Francesco Valla <valla.francesco@gmail.com>
2016-05-28 16:43:54 +0200

    Fix for inter-branch confusion

--- index.php
+++ index.php
@@ -74,7 +74,7 @@
     <link rel="stylesheet" href="js/jquery-ui.css"/>
     <script src="js/jquery.min.js" type="text/javascript"></script>
     <script src="js/jquery-ui.min.js" type="text/javascript"></script>
-    <script type="text/javascript">var printMethod = 'thermal';</script>
+    <script type="text/javascript">var printMethod = '<?php echo $CONFIG_PRINT_MODE; ?>';</script>
     <script src="js/index.js" type="text/javascript"></script>
 </head>
 <body>
